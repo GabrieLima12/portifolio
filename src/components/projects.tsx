0cefa75065be026d86da1289c2b355354086325a
GabrieLima12 <gabrieldocouto.1207@gmail.com>
2024-09-24 19:03:12 -0300

    feat: add new property to access project deployment.

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -13,9 +13,18 @@ export default function Projects () {
                         </div>
                         <div className="flex flex-col gap-5 items-center justify-center w-full sm:w-1/2">
                             <p>{project.description}</p>
-                            <a href={project.link} target="_blank">
-                                <button className="bg-black text-slate-50 dark:bg-gray-600 p-2 rounded-sm">GitHub</button>
-                            </a>
+                            <div className="flex gap-5">
+                                <a href={project.link} target="_blank">
+                                    <button className="bg-black text-slate-50 dark:bg-gray-600 p-2 rounded-sm">GitHub</button>
+                                </a>
+                                {
+                                    project.projectLink !== null && project.projectLink !== "" && (
+                                        <a href={project.projectLink} target="_blank">
+                                            <button className="bg-black text-slate-50 dark:bg-gray-600 p-2 rounded-sm">Acesse o Projeto</button>
+                                        </a>
+                                    )
+                                }
+                            </div>
                         </div>
                     </div>
                 ))}
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,6 +5,7 @@ export const projects = [
         description: "O projeto consiste em uma API Java com framework Spring para o gerenciamento de biblioteca.",
         link: "https://github.com/GabrieLima12/api-library-management",
         imgUrl: "https://raw.githubusercontent.com/GabrieLima12/api-library-management/feat/integration/src/main/resources/assets/library-img.jpg",
+        projectLink: null
     },
     {
         id: 2,
@@ -12,5 +13,14 @@ export const projects = [
         description: "Uma aplicação front-end para o genciamento de anotações utilizando React com Tailwind.",
         link: "https://github.com/GabrieLima12/nlw-notes-expert",
         imgUrl: "https://raw.githubusercontent.com/GabrieLima12/nlw-notes-expert/main/src/assets/project.png",
+        projectLink: "https://expert-notes-gold.vercel.app/"
+    },
+    {
+        id: 3,
+        name: "portifolio",
+        description: "Site para apresentar a minha pessoa e meus projetos.",
+        link: "https://github.com/GabrieLima12/portifolio",
+        imgUrl: "",
+        projectLink: null
     },
 ]
