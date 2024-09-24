06ca88605917c84ca121d2d2b38f480bdb99b390
GabrieLima12 <gabrieldocouto.1207@gmail.com>
2024-09-24 16:22:00 -0300

    fix: adjust on the home page so as not to crop the image.

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -11,12 +11,12 @@ export default function Home () {
         <section id="home" className="bg-backGray dark:bg-gray-900 dark:text-slate-50 px-10 h-screen flex flex-col gap-[50px] items-center justify-center lg:flex-row">
             {isMobile && 
                 <div className="block">
-                    <img className="max-size-[300px] mt-10 rounded-full" src={photo} alt="gabriel-photo" />
+                    <img className="max-w-[200px] lg:max-w-[300px] mt-16 rounded-full" src={photo} alt="gabriel-photo" />
                 </div>
             }
             <div className="max-w-[550px] flex flex-col gap-5 items-center lg:items-start">
-                <h1 className="text-3xl sm:text-5xl font-bold text-center lg:text-start">Desenvolvedor Full Stack</h1>
-                <p className="">Olá me chamo Gabriel, tenho 20 anos e sou um profissional na área do desenvolvimento, fico feliz em ter você aqui! <br /> Fique a vontade para olhar meus projetos e minhas habilidades.</p>
+                <h1 className="text-2xl sm:text-5xl font-bold text-center lg:text-start">Desenvolvedor Full Stack</h1>
+                <p className="text-sm">Olá me chamo Gabriel, tenho 20 anos e sou um profissional na área do desenvolvimento, fico feliz em ter você aqui! <br /> Fique a vontade para olhar meus projetos e minhas habilidades.</p>
                 <span className="flex gap-2">
                     <a href="https://www.linkedin.com/in/gabrielimacouto/" target="_blank"><FaLinkedin size={35} /></a>
                     <a href="https://github.com/GabrieLima12" target="_blank"><FaGithub size={35} /></a>
